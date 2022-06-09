a8bb279e840fcfd495e2dfb83b683089130edb8c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-09 14:43:51 -0300

    Alterando imagem do container

--- 1_imagens_e_containers/app.js
+++ 1_imagens_e_containers/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req,res)=>{
-    res.send("Minha imagem rodando");
+    res.send("Minha imagem rodando!");
 });
 
 app.listen(port,()=> console.log(`Servidor rodando na porta ${port}`));

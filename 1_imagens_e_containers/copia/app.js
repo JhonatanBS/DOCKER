1da48e155cbc2dd2efef1796e8140070f5390412
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-13 16:09:35 -0300

    Copia de imagem utilizando o cp

---
+++ 1_imagens_e_containers/copia/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res)=>{
+    res.send("Minha imagem rodando!");
+});
+
+app.listen(port,()=> console.log(`Servidor rodando na porta ${port}`));
